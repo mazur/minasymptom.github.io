2bfa51abf8caf584cc38ac8441c8408709a96807
sesam <simon.bohlin@gmail.com>
2020-04-09 05:13:28 +0000

    Deploying to gh-pages from v1.0.0 4b5acee1ec6b33d399a41bb35512b8608e66a0fc

---
+++ precache-manifest.ccf9582d439bba355cf75a6282977fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00713c78b22b07d264c960fa1093b23d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669c914d04da7692349f",
+    "url": "/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "fc32ff946385eb6af00a",
+    "url": "/static/js/2.6f4df53e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f4df53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669c914d04da7692349f",
+    "url": "/static/js/main.5200a6f8.chunk.js"
+  },
+  {
+    "revision": "45cc4609903f0bdb8a0e",
+    "url": "/static/js/runtime-main.015465a8.js"
+  }
+]);
